964543593359aaea40e923b2b9b7e0f2ddbcd11b
John Woods <john.woods@marcottelab.org>
2010-05-16 16:55:01 -0500

    Added speciesize function to String

--- lib/string_ext.rb
+++ lib/string_ext.rb
@@ -4,4 +4,24 @@ class String
     a = split("/")
     a.size == 2 ? Rational(a[0].to_i,a[1].to_i) : Rational(to_i,1)
   end
+
+  # Convert the string to a species abbreviation
+  def speciesize
+    str = self.downcase
+    if str =~ /mouse/ || str =~ /mus/ || str =~ /^mm$/
+      'Mm'
+    elsif str =~ /human/ || str =~ /homo/ || str =~ /sapiens/ || str =~ /^hs$/
+      'Hs'
+    elsif str =~ /arabidopsis/ || str =~ /thaliana/ || str =~ /^at$/
+      'At'
+    elsif str =~ /yeast/ || str =~ /saccharomyces/ || str =~ /cerev/ || str =~ /^sc$/
+      'Sc'
+    elsif str =~ /worm/ || str =~ /elegans/ || str =~ /^ce$/
+      'Ce'
+    elsif str =~ /oryza/ || str =~ /^os$/
+      'Os'
+    else
+      self
+    end
+  end
 end
